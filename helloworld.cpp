cd9d8e4e36eddbcb77a0b54fde9f263af6af1ee5
SAM111-DEVOPS <59472913+SAM111-DEVOPS@users.noreply.github.com>
2020-10-06 07:44:01 +0530

    simple hello world in c++
    simple hello world in c++,with defination of each line.

---
+++ helloworld.cpp
@@ -0,0 +1,9 @@
+#include <iostream> //it is preprocessor must declare every program
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    //HELLO WORLD
+    cout << "HEllo World"; //"COUT" is datatype which is use to print anything in c++
+
+    return 0;
+}
